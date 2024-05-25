4a4b8f4cd314f49314be5d0c0800a9f66ebb8ed5
Daniel Bachler <daniel@danielbachler.de>
2024-05-25 15:37:28 +0200

    Fixes #7667: Implement plugin for vim-indentwise
    This allows moving by indent in 3 ways:
    * jump by relative indent level (next/previous line with greater/equal/less indent)
    * jump by absolute indent level (go to next line that is at level 2)
    * jump to beginning/end of current indent block
    
    Includes tests and an update to the readme

--- src/configuration/configuration.ts
+++ src/configuration/configuration.ts
@@ -245,6 +245,7 @@ class Configuration implements IConfiguration {
   replaceWithRegister = false;
 
   smartRelativeLine = false;
+  indentwise = true;
 
   sneak = false;
   sneakUseIgnorecaseAndSmartcase = false;
